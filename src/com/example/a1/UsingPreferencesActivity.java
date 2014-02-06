bbe6501e303560403eec0068fa6467e564c78256
igoryen <igor.yentaltsev@gmail.com>
2014-02-06 18:22:26 -0500

    UsingPreferencesActivity.java: additions

--- src/com/example/a1/UsingPreferencesActivity.java
+++ src/com/example/a1/UsingPreferencesActivity.java
@@ -2,8 +2,11 @@ package com.example.a1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class UsingPreferencesActivity extends Activity {
 
@@ -13,8 +16,25 @@ public class UsingPreferencesActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
 	public void onClickLoad(View view) {
 		Intent i = new Intent("com.example.a1.AppPreferenceActivity");
 		startActivity(i);
+	}
+	
+	public void onClickDisplay(View view) {
+		SharedPreferences appPrefs = getSharedPreferences("com.example.a1.UsingPreferences_preferences", MODE_PRIVATE);
+		DisplayText(appPrefs.getString("editTextPref", ""));
+	}
+	
+	public void onClickModify(View view) {
+		SharedPreferences appPrefs = getSharedPreferences("com.example.a1.UsingPreferences_preferences", MODE_PRIVATE);
+		SharedPreferences.Editor prefsEditor = appPrefs.edit();
+		prefsEditor.putString("editTextPref", ((EditText) findViewById(R.id.txtString)).getText().toString());
+		prefsEditor.commit();
+	}
+	
+	private void DisplayText(String str) {
+		Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
 	}
 }
